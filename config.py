6bce47931e5da0a8c1647c1f7d5221b20c398c9e
jbruce <jbruce12000@gmail.com>
2018-11-27 09:08:51 -0500

    - changing slope to F degs / hour

--- config.py
+++ config.py
@@ -77,6 +77,6 @@ sim_R_ho_air   = 0.05   # K/W  " with internal air circulation
 #   Time and Temperature parameters
 
 temp_scale          = "f" # c = Celsius | f = Fahrenheit - Unit to display 
-time_scale_slope    = "m" # s = Seconds | m = Minutes | h = Hours - Slope displayed in temp_scale per time_scale_slope
+time_scale_slope    = "h" # s = Seconds | m = Minutes | h = Hours - Slope displayed in temp_scale per time_scale_slope
 time_scale_profile  = "m" # s = Seconds | m = Minutes | h = Hours - Enter and view target time in time_scale_profile
 
