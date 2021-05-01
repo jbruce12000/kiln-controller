d60cc535d0b18d66b0ae8f4784e5bde4e74d82c9
Jason Bruce <jbruce12000@gmail.com>
2021-04-30 20:27:19 -0400

    adding support to prevent integral windup

--- lib/oven.py
+++ lib/oven.py
@@ -410,7 +410,12 @@ class PID():
         error = float(setpoint - ispoint)
 
         if self.ki > 0:
-            self.iterm += (error * timeDelta * (1/self.ki))
+            if config.stop_integral_windup == True:
+                margin = setpoint * config.stop_integral_windup_margin/100
+                if (abs(error) <= abs(margin)):
+                    self.iterm += (error * timeDelta * (1/self.ki))
+            else:
+                self.iterm += (error * timeDelta * (1/self.ki))
         
         dErr = (error - self.lastErr) / timeDelta
         output = self.kp * error + self.iterm + self.kd * dErr
@@ -423,15 +428,16 @@ class PID():
         if output < 0:
             output = 0
 
-        #if output > 1:
-        #    output = 1
-
         output = float(output / window_size)
 
         if out4logs > 0:        
-            log.info("pid percents pid=%0.2f p=%0.2f i=%0.2f d=%0.2f" % (out4logs,
-                ((self.kp * error)/out4logs)*100, 
-                (self.iterm/out4logs)*100,
-                ((self.kd * dErr)/out4logs)*100)) 
+#            log.info("pid percents pid=%0.2f p=%0.2f i=%0.2f d=%0.2f" % (out4logs,
+#                ((self.kp * error)/out4logs)*100, 
+#                (self.iterm/out4logs)*100,
+#                ((self.kd * dErr)/out4logs)*100)) 
+            log.info("pid actuals pid=%0.2f p=%0.2f i=%0.2f d=%0.2f" % (out4logs,
+                self.kp * error, 
+                self.iterm,
+                self.kd * dErr)) 
 
         return output
