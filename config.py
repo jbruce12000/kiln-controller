be7f8bdc1e460ff05a9c2369b144e5293a69212e
jbruce <jason.bruce@coxautoinc.com>
2022-06-28 14:10:34 -0400

    merge automatic restart branch with master

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ log_format = '%(asctime)s %(levelname)s %(name)s: %(message)s'
 
 ### Server
 listening_ip = "0.0.0.0"
-listening_port = 8082
+listening_port = 8081
 
 ### Cost Estimate
 kwh_rate        = 0.1319  # Rate in currency_type to calculate cost to run job
