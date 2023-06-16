33b185426cca6394d692910a2361c54b12c8e851
John Pickup <home@johnpickup.com>
2023-06-16 08:54:58 +0100

    ...

--- lib/ovenDisplay.py
+++ lib/ovenDisplay.py
@@ -12,6 +12,8 @@ buffer = Image.new("RGB", (width, height))
 displayhatmini = DisplayHATMini(buffer)
 displayhatmini.set_led(0.05, 0.05, 0.05)
 draw = ImageDraw.Draw(buffer)
+fnt50 = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeSans.ttf", 50, encoding="unic")
+fnt25 = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeSans.ttf", 25, encoding="unic")
 
 class OvenDisplay(threading.Thread):
     def __init__(self,oven,ovenWatcher):
@@ -36,16 +38,21 @@ class OvenDisplay(threading.Thread):
             #update_display(oven_state)    
             time.sleep(self.sleep_time)
 
+    # {'cost': 0, 'runtime': 0, 'temperature': 23.176953125, 'target': 0, 'state': 'IDLE', 'heat': 0, 'totaltime': 0, 'kwh_rate': 0.33631, 'currency_type': '£', 'profile': None, 'pidstats': {}}
     def update_display(self, oven_state):
+        draw.rectangle((0, 0, width, height), (0, 0, 0))
         log.info(oven_state)
-        self.text(oven_state, (25, 25), 15, (255, 255, 255))
+        state = json.loads(oven_state)
+        log.info(state)
+        self.text("{0:2.1f}".format(state['temperature']), (25, 25), fnt50, (255, 255, 255))
+        self.text("{0:2.1f}".format(state['target']), (25, 75), fnt25, (255, 255, 255))
         displayhatmini.display()
 
     def send(self,oven_state):
         self.update_display(oven_state)
 
-    def text(self, text, position, size, color):
-        fnt = ImageFont.load_default()
+    def text(self, text, position, fnt, color):
+        #fnt = ImageFont.load_default()
         draw.text(position, text, font=fnt, fill=color)
 
     
